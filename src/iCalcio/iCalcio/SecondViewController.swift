87f5191c0ba5bd87ef8b0dd056ec79141cc7f8bb
Andrea Calisesi <a.calisesi@apexnet.it>
2014-09-04 18:42:18 +0200

    setup project

---
+++ src/iCalcio/iCalcio/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iCalcio
+//
+//  Created by Andrea Calisesi on 04/09/14.
+//  Copyright (c) 2014 Andrea Calisesi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
