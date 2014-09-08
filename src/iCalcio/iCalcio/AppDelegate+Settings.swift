5e1bd2e405e63ce93be7d403683fbf7b108f0841
Andrea Calisesi <a.calisesi@apexnet.it>
2014-09-08 13:07:25 +0200

    Ignore api urls

---
+++ src/iCalcio/iCalcio/AppDelegate+Settings.swift
@@ -0,0 +1,9 @@
+//
+//  AppDelegate+Settings.swift
+//  iCalcio
+//
+//  Created by Andrea Calisesi on 08/09/14.
+//  Copyright (c) 2014 Andrea Calisesi. All rights reserved.
+//
+
+import Foundation
