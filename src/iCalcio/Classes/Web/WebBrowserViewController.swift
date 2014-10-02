68c62a5ecd6641ea7224dc3e7c1c410ce74b231a
Andrea Calisesi <a.calisesi@apexnet.it>
2014-10-02 12:31:26 +0200

    Enable banner container for each controls

--- src/iCalcio/Classes/Web/WebBrowserViewController.swift
+++ src/iCalcio/Classes/Web/WebBrowserViewController.swift
@@ -9,14 +9,12 @@
 import UIKit
 import iAd
 
-class WebBrowserViewController: UIViewController, UIWebViewDelegate, ADBannerViewDelegate {
+class WebBrowserViewController: UIViewController, UIWebViewDelegate{
 
     var browserTitle: String = ""
     var navigationUrl: String = ""
     
     var isNavBarEnabled: Bool = false
-    
-    private var adBannerView: ADBannerView!
     
     private var segmentControl : UISegmentedControl!
     
@@ -38,9 +36,6 @@ class WebBrowserViewController: UIViewController, UIWebViewDelegate, ADBannerVie
         self.webView!.loadRequest(req)
         self.webView!.scalesPageToFit = true
         
-        // setup Ads
-        self.loadAds()
-        
         // init navbar for navigation
         self.initNavBar()
         
@@ -49,18 +44,11 @@ class WebBrowserViewController: UIViewController, UIWebViewDelegate, ADBannerVie
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
-        self.layoutAnimated(false)
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    override func viewDidLayoutSubviews() {
-        self.layoutAnimated(UIView.areAnimationsEnabled())
-        
     }
     
     override func viewDidDisappear(animated: Bool) {
@@ -113,83 +101,6 @@ class WebBrowserViewController: UIViewController, UIWebViewDelegate, ADBannerVie
             self.segmentControl.setEnabled(self.webView.canGoForward, forSegmentAtIndex: 1)
         }
         
-    }
-    
-    // MARK: - iAd banner managment
-    private func loadAds(){
-        adBannerView = ADBannerView(adType: ADAdType.Banner)
-        adBannerView.delegate = self
-        view.addSubview(adBannerView)
-    }
-
-    private func layoutAnimated(animated: Bool){
-        
-        // offset for tabbar
-        let offsetHeightTabBar:CGFloat = 50
-        
-        // contentFrame as view bounds
-        var contentFrame: CGRect = self.view.bounds
-
-        // all we need to do is ask the banner for a size that fits into the layout area we are using
-        var sizeForBanner: CGSize = adBannerView.sizeThatFits(contentFrame.size)
-
-        // compute the ad banner frame
-        var bannerFrame: CGRect = adBannerView.frame
-        if adBannerView.bannerLoaded {
-            // bring the ad into view
-            contentFrame.size.height -= sizeForBanner.height // shrink down content frame to fit the banner below it
-            bannerFrame.origin.y = contentFrame.size.height - offsetHeightTabBar
-            bannerFrame.size.height = sizeForBanner.height
-            bannerFrame.size.width = sizeForBanner.width
-            // if the ad is available and loaded, shrink down the content frame to fit the banner below it,
-            // we do this by modifying the vertical bottom constraint constant to equal the banner's height
-            //
-            var verticalBottomConstraint:NSLayoutConstraint = self.bottomConstraint
-            verticalBottomConstraint.constant = sizeForBanner.height
-            self.view.layoutSubviews()
-        
-        }
-        else {
-            // hide the banner off screen further off the bottom
-            bannerFrame.origin.y = contentFrame.size.height
-            
-        }
-        
-        UIView.animateWithDuration(animated ? 0.25 : 0.0, animations: {
-            self.webView.frame = contentFrame
-            self.webView.layoutIfNeeded()
-            self.adBannerView.frame = bannerFrame
-            }, completion: {
-                (value: Bool) in
-                //println(">>> Animation done.")
-        })
-
-        
-    }
-    
-    // MARK: - ADBannerViewDelegate
-    func bannerViewDidLoadAd(banner: ADBannerView!) {
-        println("bannerViewDidLoadAd")
-        
-        self.layoutAnimated(true)
-    }
-
-    func bannerView(banner: ADBannerView!,
-        didFailToReceiveAdWithError error: NSError!) {
-        println("didFailToReceiveAdWithError: \(error.localizedDescription)")
-            
-        self.layoutAnimated(true)
-    }
-
-    func bannerViewActionDidFinish(banner: ADBannerView!) {
-        println("bannerViewActionDidFinish: banner view is finished an ad action")
-    }
-    
-    func bannerViewActionShouldBegin(banner: ADBannerView!,
-        willLeaveApplication willLeave: Bool) -> Bool {
-
-        println("bannerViewActionShouldBegin: banner view is beginning an ad action")
-        return true
     }
     
     // MARK: - webView Delegate
